3b9dbb3ee68ee08fef0a60511af61bcdd55fd0fb
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-09 13:29:32 -0600

    add Monkeys Spinning Monkeys modified:   lib.js new file:   music2.mp3 modified:   musyconfig.html modified:   web/index.html modified:   web/lib.js new file:   web/music2.mp3

--- lib.js
+++ lib.js
@@ -2,4 +2,9 @@ function playMusic1() {
 player = document.getElementById("player")
 player.src="music1.mp3"
 }
+function playMusic2() {
+    player = document.getElementById("player")
+    player.src="music2.mp3"
+}
 document.getElementById("music").addEventListener("click", playMusic1);
+document.getElementById("music2").addEventListener("click", playMusic2);
--- web/lib.js
+++ web/lib.js
@@ -2,3 +2,7 @@ function playMusic1() {
 player = document.getElementById("player")
 player.src="music1.mp3"
 }
+function playMusic2() {
+    player = document.getElementById("player")
+    player.src="music2.mp3"
+}
