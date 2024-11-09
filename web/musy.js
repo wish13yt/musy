825a7914c17ebd8d8d2842c431840f7ca57de900
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-08 19:37:37 -0600

    add test mode to web title

--- web/musy.js
+++ web/musy.js
@@ -4,4 +4,8 @@ function runMusy() {
     player.src=murl;
     event.preventDefault();
 }
+function testMode() {
+    player = document.getElementById("player")
+    player.src="https://www.onlinemictest.com/wp-content/themes/onlinemictest/sound.mp3";
+}
 document.getElementById("btn").addEventListener("click", runMusy);
