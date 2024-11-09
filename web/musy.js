646b62f02158a404f88040f0b30742d72256fa52
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-08 19:40:00 -0600

    remove test mode broke after 5 seconds

--- web/musy.js
+++ web/musy.js
@@ -4,8 +4,4 @@ function runMusy() {
     player.src=murl;
     event.preventDefault();
 }
-function testMode() {
-    player = document.getElementById("player")
-    player.src="https://www.onlinemictest.com/wp-content/themes/onlinemictest/sound.mp3";
-}
 document.getElementById("btn").addEventListener("click", runMusy);
