c434ab342a35f152f465959ee97d3da2808da1f8
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-09 13:49:52 -0600

    add New Friendly 	modified:   lib.js 	new file:   music3.mp3 	modified:   musyconfig.html 	modified:   web/index.html 	modified:   web/lib.js 	new file:   web/music3.mp3

--- lib.js
+++ lib.js
@@ -6,5 +6,10 @@ function playMusic2() {
     player = document.getElementById("player")
     player.src="music2.mp3"
 }
+function playMusic3() {
+    player = document.getElementById("player")
+    player.src="music3.mp3"
+}
 document.getElementById("music").addEventListener("click", playMusic1);
 document.getElementById("music2").addEventListener("click", playMusic2);
+document.getElementById("music3").addEventListener("click", playMusic3);
--- web/lib.js
+++ web/lib.js
@@ -6,3 +6,7 @@ function playMusic2() {
     player = document.getElementById("player")
     player.src="music2.mp3"
 }
+function playMusic3() {
+    player = document.getElementById("player")
+    player.src="music3.mp3"
+}
