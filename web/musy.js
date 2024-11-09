70d6c481adc0d9e3acffa01936922cb8eb4992c3
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-08 19:41:54 -0600

    optimize for web use removes some of the extension's restriction bypasses

--- web/musy.js
+++ web/musy.js
@@ -2,6 +2,4 @@ function runMusy() {
     murl = document.getElementById("musy").value
     player = document.getElementById("player")
     player.src=murl;
-    event.preventDefault();
 }
-document.getElementById("btn").addEventListener("click", runMusy);
